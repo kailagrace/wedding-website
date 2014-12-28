d55cdde166847eb06ed75a87183390f3aee2a20e
Kaila Grace <kaila.manca@gmail.com>
2014-12-28 02:00:01 -0500

    modernizr

--- js/interval.js
+++ js/interval.js
@@ -1,42 +1,46 @@
 $(document).ready(function() {
   
+  if( Modernizr.mq('only screen and (min-height: 640px)') ) {
+  // only executed if screen height is > 640px
 
-  var rotate = function() {
-  $("html")
-    .delay(8000).queue(function() {
-        $(this).removeClass("background-image-one");
-        $(this).addClass("background-image-two");
-        $(this).dequeue();
-          next();
-    })
-    .delay(8000).queue(function() {
-        $(this).removeClass("background-image-two");
-        $(this).addClass("background-image-three");
-        $(this).dequeue();
-          next();
-    })
-    .delay(11000).queue(function(next) {
-        $(this).removeClass("background-image-three");
-        $(this).addClass("background-image-four");
-        $(this).dequeue();
-          next();
-     
-    })
-    .delay(8000).queue(function(next) {
-        $(this).removeClass("background-image-four");
-        $(this).addClass("background-image-five");
-        $(this).dequeue();
-    
-    })
-    .delay(8000).queue(function(next) {
-        $(this).removeClass("background-image-five");
-        $(this).addClass("background-image-one");
-        $(this).dequeue();
-        next();
-    })
-    .queue(rotate);
+      var rotate = function() {
+      $("html")
+        .delay(8000).queue(function() {
+            $(this).removeClass("background-image-one");
+            $(this).addClass("background-image-two");
+            $(this).dequeue();
+              next();
+        })
+        .delay(8000).queue(function() {
+            $(this).removeClass("background-image-two");
+            $(this).addClass("background-image-three");
+            $(this).dequeue();
+              next();
+        })
+        .delay(11000).queue(function(next) {
+            $(this).removeClass("background-image-three");
+            $(this).addClass("background-image-four");
+            $(this).dequeue();
+              next();
+         
+        })
+        .delay(8000).queue(function(next) {
+            $(this).removeClass("background-image-four");
+            $(this).addClass("background-image-five");
+            $(this).dequeue();
+        
+        })
+        .delay(8000).queue(function(next) {
+            $(this).removeClass("background-image-five");
+            $(this).addClass("background-image-one");
+            $(this).dequeue();
+            next();
+        })
+        .queue(rotate);
 };
 
-rotate();
+    rotate();
+
+}
 
 });
